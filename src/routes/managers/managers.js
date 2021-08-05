14175ff71d3e213a7dfe5f3d2b820553e5eb136f
Sri Kakarla <31450756+charanksri@users.noreply.github.com>
2021-08-04 21:33:13 -0400

    Update managers.js

--- src/routes/managers/managers.js
+++ src/routes/managers/managers.js
@@ -8,7 +8,7 @@ https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#2-add-man
 
 export const managers = [
   {
-    "roster": 1, // ID of the roster that the manager manages (look at the order of the power rankings graph)
+    "roster": 9, // ID of the roster that the manager manages (look at the order of the power rankings graph)
     "name": "Charan ",
     "tookOver": null // (optional) used if a manager took over a team, delete this line or change to null otherwise
     "location": "Fort Myers", // (optional)
