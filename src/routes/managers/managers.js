f90a042ebf3d95b8f34f97a5316211e4ca1eb7bd
Sri Kakarla <31450756+charanksri@users.noreply.github.com>
2021-08-04 22:29:25 -0400

    Update managers.js

--- src/routes/managers/managers.js
+++ src/routes/managers/managers.js
@@ -235,51 +235,5 @@ export const managers = [
     "philosophy": "Your fantasy team's philosophy",
     "tradingScale": 10, // 1 - 10
     "preferredContact": "Sleeper", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone' and 'Carrier Pigeon' are currently supplied in the template
-  },
-    
-     {
-    "roster": 11, // ID of the roster that the manager manages (look at the order of the power rankings graph)
-    "name": "Charan ",
-    "tookOver": null // (optional) used if a manager took over a team, delete this line or change to null otherwise
-    "location": "Fort Myers", // (optional)
-    "bio": "Lorem ipsum...",
-    "photo": "/managers/question.jpg", // square ratio recommended (no larger than 500x500)
-    "fantasyStart": 2019, // (optional) when did the manager start playing fantasy football
-    "favoriteTeam": "mia", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
-    "mode": "Win Now", // (optional) 'Win Now' or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
-    "rival": {
-      name: "Guardians", // Can be anything (usually your rival's name)
-      link: 6, // manager array number within this array, or null to link back to all managers page
-      image: "/managers/question.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
-    },
-    "favoritePlayer": 4046, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
-    "valuePosition": "RB", // (optional) Favorite position (QB, WR, RB, TE, etc.)
-    "rookieOrVets": "Rookies", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
-    "philosophy": "Your fantasy team's philosophy",
-    "tradingScale": 10, // 1 - 10
-    "preferredContact": "Sleeper", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone' and 'Carrier Pigeon' are currently supplied in the template
-  },
-    
-     {
-    "roster": 12, // ID of the roster that the manager manages (look at the order of the power rankings graph)
-    "name": "Charan ",
-    "tookOver": null // (optional) used if a manager took over a team, delete this line or change to null otherwise
-    "location": "Fort Myers", // (optional)
-    "bio": "Lorem ipsum...",
-    "photo": "/managers/question.jpg", // square ratio recommended (no larger than 500x500)
-    "fantasyStart": 2019, // (optional) when did the manager start playing fantasy football
-    "favoriteTeam": "mia", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
-    "mode": "Win Now", // (optional) 'Win Now' or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
-    "rival": {
-      name: "Guardians", // Can be anything (usually your rival's name)
-      link: 6, // manager array number within this array, or null to link back to all managers page
-      image: "/managers/question.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
-    },
-    "favoritePlayer": 4046, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
-    "valuePosition": "RB", // (optional) Favorite position (QB, WR, RB, TE, etc.)
-    "rookieOrVets": "Rookies", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
-    "philosophy": "Your fantasy team's philosophy",
-    "tradingScale": 10, // 1 - 10
-    "preferredContact": "Sleeper", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone' and 'Carrier Pigeon' are currently supplied in the template
-  }      
+  }    
 ]
