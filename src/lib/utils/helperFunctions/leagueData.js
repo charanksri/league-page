e2ac75b8038acc2bfd240f157a69e27611e3f608
Sri Kakarla <31450756+charanksri@users.noreply.github.com>
2021-08-05 08:26:17 -0400

    Update leagueData.js

--- src/lib/utils/helperFunctions/leagueData.js
+++ src/lib/utils/helperFunctions/leagueData.js
@@ -1,8 +1,8 @@
 import { get } from 'svelte/store';
 import {leagueData} from '$lib/stores';
 
-export const leagueID = "534406247646912512"; // your league ID
-export const leagueName = "Telugu Tejalu"; // your league name
+export const leagueID = "704500689639919616"; // your league ID
+export const leagueName = "Infamous League"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 
