f14d7a3763efc04fc84d576b1d52c8ec034babcd
Sri Kakarla <31450756+charanksri@users.noreply.github.com>
2021-08-04 21:04:30 -0400

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -155,12 +155,10 @@
     <div id="main">
         <div class="text">
             <h6>{leagueName}</h6>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>Founded in 2019, the Telugu Tejalu league began as a redraft league amongst a group of six friends. The league added four more teams in 2020, and two more teams for the upcoming 2021 season which makes it a full-fledged 12 team league. In 2021, the league converted into a keeper league along with many other scoring changes to keep it exciting.</p>
+            <p>Some managers haven't watched a single match prior to participating in the league. It is only after they started playing in the league, they learned about how an nfl game works....the basic rules, the positions and the scoring. A manager drafted Tom Brady in the very first pick of our first draft. That should tell you enough about how this league started.</p>
+            <p>Despite these starting troubles, the league managed to learn a lot about the game. While the winning managers believe in strategy, losing managers blame it on the luck involved in the game to win.</p>
+            <p>While the ultimate prize is bragging rights and sending obnoxious texts to the rest of the league for the next year, the champion manager also walks away with a cool fantasy football ring and a cash prize of $50.</p>
         </div>
         <PowerRankings />
     </div>
