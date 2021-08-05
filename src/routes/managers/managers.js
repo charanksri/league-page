b9e3b7f9d5878a5a75f6ad308f4c96966a5e8221
Sri Kakarla <31450756+charanksri@users.noreply.github.com>
2021-08-05 19:17:34 -0400

    Update managers.js

--- src/routes/managers/managers.js
+++ src/routes/managers/managers.js
@@ -82,7 +82,7 @@ export const managers = [
     "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
     "location": "Chennai", // (optional)
     "bio": "Lorem ipsum...",
-    "photo": "/managers/monstars.jpg", // square ratio recommended (no larger than 500x500)
+    "photo": "/managers/monstars.png", // square ratio recommended (no larger than 500x500)
     "fantasyStart": 2019, // (optional) when did the manager start playing fantasy football
     "favoriteTeam": "dal", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
     "mode": "Win Now", // (optional) 'Win Now' or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
